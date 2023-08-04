c837366804c59c0ed89c86e8155de4ba2dd34027
jmescolano <escolanojademark17@outlook.com>
2023-08-04 15:19:22 +0800

    Add project files.

---
+++ qrCode/qrCode/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace qrCode
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Result:", result.Text, "OK");
+
+            });
+        }
+    }
+}
